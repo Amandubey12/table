fac0d923bb88b2fca24d04c35c67ab2f5c69f637
Amandubey12 <76807303+Amandubey12@users.noreply.github.com>
2021-11-06 14:39:15 +0530

    update

---
+++ table.js
@@ -0,0 +1,34 @@
+function myCreateFunction() {
+    var table = document.getElementById("myTable");
+    var row = table.insertRow(2);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
+
+    cell1.innerHTML = "1";
+    cell2.innerHTML =  "";
+    cell3.innerHTML = "";
+    cell4.innerHTML = "";
+    cell5.innerHTML = "";
+    }
+
+function myFunction() {
+    var table = document.getElementById("myTable");
+    var row = table.insertRow(7);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
+
+    cell1.innerHTML = "7";
+    cell2.innerHTML = "";
+    cell3.innerHTML = "";
+    cell4.innerHTML = "";
+    cell5.innerHTML = "";
+    
+
+   
+}
